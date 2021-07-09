6738d67523fc4b76255597c4b4028174949f68c8
Luan Persini <luanpersini@hotmail.com>
2021-07-09 16:53:48 -0300

    feat: should return 500 if openAccount throws

--- src/user-interface/api/controllers/bank-account/open-account/open-account-controller.ts
+++ src/user-interface/api/controllers/bank-account/open-account/open-account-controller.ts
@@ -1,34 +1,20 @@
 import { HttpResponse, HttpRequest, Controller } from './open-account-controller-protocols'
-// import { serverError } from '@/user-interface/common/helpers/http/http-helper'
 import { OpenAccount } from '@/core/components/bank-account/usecases/open-account/open-account'
-// import { serverError } from '@/user-interface/common/helpers/http/http-helper'
-// import { LoadAccountByToken } from '@/core/components/user-account/usecases/load-account-by-token/load-account-by-token'
-
-// 2. - [ ] **Valida** se já existe uma conta bancária com o userId fornecido
-// 3. - [ ] Cria uma **conta bancária** para o usuário logado
-// 4. - [ ] Retorna **200** com os dados da conta bancária criada
+import { serverError } from '@/user-interface/common/helpers/http-helper'
 
 export class OpenAccountController implements Controller {
   constructor (
     private readonly openAccount: OpenAccount
-    // private readonly loadAccountByToken: LoadAccountByToken
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { userId } = httpRequest
-    await this.openAccount.open(userId)
-    return null
+    try {
+      const { userId } = httpRequest
+      await this.openAccount.open(userId)
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
-
-/*
-try {
-  const { userId } = httpRequest.body
-  await this.openAccount.open(userId)
-  return null
-} catch (error) {
-  return serverError(error)
-}
-const bankAccount = await this.openAccount.open(userId)
-}
-*/
+// const bankAccount = await this.openAccount.open(userId)
