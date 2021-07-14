3bf21b9a31797a5426f83eefc9ea2262b8c24c20
Luan Persini <luanpersini@hotmail.com>
2021-07-14 11:41:49 -0300

    refactor: bankAccountMongoRepository - return result without id map

--- src/core/components/bank-account/repositories/infra.repository-implementation/bank-account-mongo-repository.ts
+++ src/core/components/bank-account/repositories/infra.repository-implementation/bank-account-mongo-repository.ts
@@ -1,6 +1,5 @@
 import { BankAccountModel } from '@/core/components/bank-account/domain/bank-account'
 import { MongoHelper } from '@/infra/persistence/db/helpers/mongo-helper'
-// import { MongoHelper } from '@/infra/persistence/db/helpers/mongo-helper'
 import { LoadAccountByUserIdRepository } from '../load-bank-account-by-userid-repository'
 import { OpenAccountRepository } from '../open-account-repository'
 
@@ -8,7 +7,7 @@ export class BankAccountMongoRepository implements OpenAccountRepository, LoadAc
   async open (bankAccount: BankAccountModel): Promise<BankAccountModel> {
     const bankAccountCollection = await MongoHelper.getCollection('bank-account')
     const result = await bankAccountCollection.insertOne(bankAccount)
-    return MongoHelper.map(result.ops[0])
+    return result.ops[0]
   }
 
   async loadByUserId (userId: string): Promise<BankAccountModel> {
