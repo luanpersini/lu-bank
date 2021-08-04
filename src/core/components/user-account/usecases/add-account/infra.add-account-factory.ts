1ce3b5ca764b80b0141a5410f0bb4a74d6e64328
Luan Persini <luanpersini@hotmail.com>
2021-08-04 11:56:03 -0300

    refactor: removed extra adapters used to substituition low coupling example

--- src/core/components/user-account/usecases/add-account/infra.add-account-factory.ts
+++ src/core/components/user-account/usecases/add-account/infra.add-account-factory.ts
@@ -1,11 +1,11 @@
 import { DbAddAccount } from '@/core/components/user-account/usecases/add-account/db-add-account'
 import { AddAccount } from '@/core/components/user-account/usecases/add-account/add-account'
 import { AccountMongoRepository } from '@/core/components/user-account/infra.repository-implementations/account-mongo-repository'
-import { BcryptJsAdapter } from '@/infra/adapters/hasher/bcryptjs-adapter/bcryptjs-adapter'
+import { BcryptAdapter } from '@/infra/adapters/hasher/bcrypt-adapter/bcrypt-adapter'
 
 export const makeDbAddAccount = (): AddAccount => {
   const salt = 12
-  const bcryptAdapter = new BcryptJsAdapter(salt)
+  const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   return new DbAddAccount(bcryptAdapter, accountMongoRepository, accountMongoRepository)
 }
--- src/infra/server/server.ts
+++ src/infra/server/server.ts
@@ -2,16 +2,17 @@ import 'module-alias/register'
 import env from './config/env'
 import { MongoHelper } from '@/infra/persistence/db/helpers/mongo-helper'
 
-async function startServer (): Promise<void> {
-  console.log(process.env.NODE_ENV)
-  const app = (await import('./config/app')).default
-  app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+// async function startServer (): Promise<void> {
+//   console.log(process.env.NODE_ENV)
+//   const app = (await import('./config/app')).default
+//   app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+//   await MongoHelper.connect(env.mongoUrl)
+// }
+// startServer().then().catch(e => console.log(e))
 
-  if (process.env.NODE_ENV === 'development') {
-    await MongoHelper.connect(env.mongoDevUrl)
-  }
-  if (process.env.NODE_ENV === 'production') {
-    await MongoHelper.connect(env.mongoUrl)
-  }
-}
-startServer().then().catch(e => console.log(e))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
